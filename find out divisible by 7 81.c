e07a4869d5037a3c22164d3f000da7f166f555a8
krixhiv21 <valibenchaudhari0@gmail.com>
2025-09-05 23:09:26 +0530

    Add files via upload

---
+++ find out divisible by 7 81.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    // Input the number
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    // Check divisibility by 7
+    if (number % 7 == 0) {
+        printf("%d is divisible by 7.\n", number);
+    } else {
+        printf("%d is not divisible by 7.\n", number);
+    }
+
+    return 0;
+}
